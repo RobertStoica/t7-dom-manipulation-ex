6638c35314ec0fc250a8b22b7317010bfa75bc57
Robert Stoica <robe3750@stud.kea.dk>
2022-08-30 21:48:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //Write your own code here
 //DON'T touch any other files!
+//1 select them all
+const brs = document.querySelectorAll("br");
+console.log(brs);
+//2 loop through them
+brs.forEach(removeElement);
+//3 remove() them
+function removeElement(element) {
+  element.remove();
+}
+// give all sections class filled
+//1
+const sections = document.querySelectorAll("section");
+console.log(sections);
+//2
+sections.forEach(addClass);
+//3
+function addClass(elm) {
+  elm.classList.add("filled");
+}
+//.classList.add("filled");
+// Remove all elements that come before section
+sections.forEach(removeElementBefore);
+
+function removeElementBefore(item) {
+  item.previousElementSibling.remove();
+}
